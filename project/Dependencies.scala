568762182d10c975b8e1cdb4f578a601a79e2cd3
velocorner <velocorner.com@gmail.com>
2026-04-04 00:17:08 +0200

    bump BE libs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,9 +7,9 @@ object Dependencies {
   val catsEffectVersion = "3.7.0"
   val mouseVersion = "1.4.0"
   val http4s = "0.23.33"
-  val tapirVersion = "1.13.14"
+  val tapirVersion = "1.13.15"
   val playWsVersion = "3.0.10" // standalone version
-  val pekkoVersion = "1.4.0"
+  val pekkoVersion = "1.5.0"
   val playJsonVersion = "3.0.6"
   val shapelessVersion = "2.3.12"
   val logbackVersion = "1.5.32"
